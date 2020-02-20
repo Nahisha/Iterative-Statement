f515324b23c865d26d76aca829a4d215edfa80c4
Nahisha <Nahisha@DESKTOP-D87FS9K>
2020-02-19 19:49:54 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Iterative_Statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input;
+            
+            Console.WriteLine("Hello, please enter a value between 2 and 10.");
+            input = int.Parse(Console.ReadLine());
+
+            if (input%2 == 1)
+            {
+                for(int j = 0; j < input-1; j++)
+                {
+                    int temp = j;
+                    temp++;
+                    Console.WriteLine(temp + ". You entered an odd number.");
+                }     
+            }
+            
+            else
+            {
+                for (int i = 0; i < input+1; i++)
+                {
+                    int temp = i;
+                    temp++;
+                    Console.WriteLine(temp + ". You entered an even number.");
+                }
+            }
+        }
+    }
+}
